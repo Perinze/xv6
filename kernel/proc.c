bff9aafdc62345c46eca5e10dd5ddf64f01fca88
perinze <perinzefaper@foxmail.com>
2022-07-14 23:03:51 +0800

    usyscall page

--- kernel/proc.c
+++ kernel/proc.c
@@ -207,7 +207,7 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  // map the read-only page at USYSCALL for struct usyscall.
+  // map the read-only page at USYSCALL just below TRAPFRAME.
   if(mappages(pagetable, USYSCALL, PGSIZE,
               (uint64)(p->usyscall), PTE_R) < 0){
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
@@ -226,6 +226,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
