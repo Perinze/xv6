951c2e1383c64585ff1855dcec08e5ef4d6bb774
perinze <perinzefaper@foxmail.com>
2022-07-15 17:24:30 +0800

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable, int level)
+{
+  if(level == 0)
+    printf("page table %p\n", (uint64)pagetable);
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V){
+    // this PTE is valid
+      for(int j = 0; j < level+1; j++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, (uint64)pte, (uint64)PTE2PA(pte));
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        // this PTE points to a lower-level page table.
+        uint64 child = PTE2PA(pte);
+        vmprint((pagetable_t)child, level+1);
+      }
+    }
+  }
+}
