ddb80a93bd5a5d20736d46b258c39ac90a45c0bc
perinze <perinzefaper@foxmail.com>
2022-07-22 11:52:23 +0800

    add copyonwrite to copyout

--- kernel/vm.c
+++ kernel/vm.c
@@ -358,9 +358,19 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
+  pte_t *pte;
+  uint flags;
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+    if((pte = walk(pagetable, va0, 0)) == 0)
+      return -1;
+    pa0 = PTE2PA(*pte);
+    flags = PTE_FLAGS(*pte);
+    if(flags & PTE_COW)
+      if(copyonwrite(pagetable, va0) < 0)
+        return -1;
+
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
