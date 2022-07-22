9978ccbdb9e485e5d59e2c8966c125ca88120b01
perinze <perinzefaper@foxmail.com>
2022-07-22 23:46:45 +0800

    implement pageref better, and add a freepage variable

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -15,6 +15,7 @@ extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
 int pageref[PHYSTOP / PGSIZE];
+uint64 freepage = 0;
 
 struct run {
   struct run *next;
@@ -54,10 +55,9 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Decrease reference count
   if(pageref[i] > 0)
-    if(--pageref[i] > 0)
-      return;
+    return;
+  freepage++;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -80,13 +80,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    --freepage;
+  }
   release(&kmem.lock);
 
   if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
-    pageref[(uint64)r / PGSIZE] = 1;
+    pageref[(uint64)r / PGSIZE] = 0;
   }
   return (void*)r;
 }
