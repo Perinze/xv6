00892c33f074dcba6f286e05eaddc1807fadfe1e
perinze <perinzefaper@foxmail.com>
2022-08-18 22:59:16 +0800

    barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -22,15 +22,17 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
-static void 
+static void
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  assert(pthread_mutex_lock(&bstate.barrier_mutex) == 0);
+  if (++bstate.nthread == nthread) {
+    bstate.round++;
+    bstate.nthread = 0;
+    assert(pthread_cond_broadcast(&bstate.barrier_cond) == 0);
+  } else
+    assert(pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex) == 0);
+  assert(pthread_mutex_unlock(&bstate.barrier_mutex) == 0);
 }
 
 static void *
